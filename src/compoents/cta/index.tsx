7d9075abc261a74209d9af3830b3776bd4895090
maazu <maaz@crewfellows.com>
2025-05-20 23:08:45 +0100

    feat: cta-2

--- src/compoents/cta/index.tsx
+++ src/compoents/cta/index.tsx
@@ -3,79 +3,71 @@ import {
   Container,
   Text,
   Heading,
-  VStack,
-  HStack,
   Button,
-  Stack,
-  Input,
+  Image,
+  SimpleGrid,
+  GridItem,
 } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
 
 const CallToAction = () => {
+  const router = useRouter();
   return (
-    <Box
-      //   bg='accent'
-      //   backgroundImage='linear-gradient(135deg, rgba(29, 99, 175, 0.95) 0%, rgba(29, 99, 175, 0.8) 100%)'
-      borderRadius='xl'
-      overflow='hidden'
-      boxShadow='lg'
-      position='relative'
-    >
+    <Box borderRadius='xl' overflow='hidden' boxShadow='lg' position='relative'>
       <Container maxW='7xl' py={12}>
-        <Stack
-          direction={{ base: 'column', md: 'row' }}
-          gap={8}
-          align={{ base: 'center', md: 'center' }}
-          justify='space-between'
-        >
-          <VStack gap={4} align='flex-start' maxW={{ base: 'full', md: '50%' }}>
-            <Heading
-              color='primary'
-              fontSize={{ base: '2xl', md: '3xl' }}
-              lineHeight='1.2'
-            >
-              Ready to Connect with Your Communities?
-            </Heading>
-            <Text fontSize={{ base: 'md', md: 'lg' }}>
-              Start creating authentic connections with audiences across the
-              cultural spectrum. Our team is ready to craft a customized
-              strategy for your brand.
-            </Text>
-          </VStack>
+        <SimpleGrid columns={{ base: 1, md: 2 }} gap={10}>
+          <GridItem spaceY={5}>
+            <Image
+              src='/common/imgs/cta-picture.png'
+              alt='Spectrum Media Team'
+              objectFit='cover'
+              height='60'
+              width='100%'
+            />
 
-          <VStack gap={4} align='stretch' w={{ base: 'full', md: 'auto' }}>
-            <HStack>
-              <Input
-                placeholder='Enter your email'
-                bg='white'
-                color='gray.700'
-                // border='none'
-                _placeholder={{ color: 'gray.400' }}
-                size='lg'
-                rounded='md'
-              />
-              <Button
-                bg={'primary'}
-                color='white'
-                size='lg'
-                px={6}
-                fontWeight='bold'
-                rounded='md'
-                _hover={{ bg: '#d13428' }}
-              >
-                Get Started
-              </Button>
-            </HStack>
-            <Text
-              fontSize='sm'
-              //   color='white'
-              opacity={0.8}
-              textAlign={{ base: 'center', md: 'left' }}
-            >
-              Join over 500+ brands connecting authentically with diverse
-              audiences.
+            <Heading size='2xl'>Partner with us</Heading>
+            <Text>
+              Tell us about your project, and let&#39;s build the future
+              together.
             </Text>
-          </VStack>
-        </Stack>
+            <Button
+              bg='primary'
+              color='white'
+              size='lg'
+              _hover={{ bg: 'primaryHover' }}
+              onClick={() => {
+                router.push('/contact-us');
+              }}
+            >
+              Contact us
+            </Button>
+          </GridItem>
+          <GridItem spaceY={5}>
+            <Image
+              src='/common/imgs/cta-2.png'
+              alt='Spectrum Media Team'
+              objectFit='cover'
+              height='60'
+              width='100%'
+            />
+
+            <Heading size='2xl'>Lead and grow</Heading>
+            <Text>
+              Imagine a career as unique as you are. With us, it&#39;s possible.
+            </Text>
+            <Button
+              bg='primary'
+              color='white'
+              size='lg'
+              _hover={{ bg: 'primaryHover' }}
+              onClick={() => {
+                router.push('/contact-us');
+              }}
+            >
+              Join us
+            </Button>
+          </GridItem>
+        </SimpleGrid>
       </Container>
     </Box>
   );
